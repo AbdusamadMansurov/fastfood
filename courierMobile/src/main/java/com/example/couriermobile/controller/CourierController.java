3f5f013cef0ba761654fe2a3e37098d02b2c1639
Abdusamad <abdusamat.mansurov@gmail.com>
2022-06-24 15:58:28 +0500

    commit

--- courierMobile/src/main/java/com/example/couriermobile/controller/CourierController.java
+++ courierMobile/src/main/java/com/example/couriermobile/controller/CourierController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.ws.rs.Path;
 import java.util.List;
 
 @RestController
